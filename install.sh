fc2675d56d3e85626ee2928a614624cbcac3649f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-23 01:59:54 +0800

    Update config
    - check for `Brewfile` existence
    - add tmux configuration
    - skip `chsh`, it doesn't work :(

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [[ ! -d /home/linuxbrew/.linuxbrew ]] || [[ ! -x /home/linuxbrew/.linuxbrew/b
     bash -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if ! brew bundle check; then
+if [[ -r Brewfile ]] && ! brew bundle check; then
     brew bundle install
 fi
 
@@ -19,6 +19,7 @@ if [[ -f ~/.bashrc ]]; then
     grep EDITOR ~/.bashrc || echo "export EDITOR=nvim" >> ~/.bashrc
     if ! grep TMUX_TMPDIR ~/.bashrc; then
         cat <<'BASH' >> ~/.bashrc
+command -v zsh &>/dev/null && exec zsh
 export TMUX_TMPDIR="${HOME}"
 if [[ -z "${TMUX}" ]] && command -v tmux &>/dev/null; then
     exec tmux new-session -A -s main
@@ -27,9 +28,6 @@ BASH
     fi
 fi
 
-install -b .zshrc "${HOME}/.zshrc"
-install -b .p10k.zsh "${HOME}/.p10k.zsh"
-
 curl --fail --silent --show-error --location --output omz-install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 
 bash omz-install.sh --skip-chsh --unattended --keep-zshrc
@@ -37,7 +35,10 @@ bash omz-install.sh --skip-chsh --unattended --keep-zshrc
 export ZSH_CUSTOM="${HOME}/.config/zsh/custom"
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
-if zsh="$(command -v zsh 2>/dev/null)"; then
-    sudo chsh -s "${zsh}"
-fi
+install -b -m 644 .zshrc "${HOME}/.zshrc"
+install -b -m 644 .p10k.zsh "${HOME}/.p10k.zsh"
+install -b -m 644 tmux.conf "${HOME}/.config/tmux/tmux.conf"
+
+~/.config/tmux/plugins/tpm/bin/install_plugins
